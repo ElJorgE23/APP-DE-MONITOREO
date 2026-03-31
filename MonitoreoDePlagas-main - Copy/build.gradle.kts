e85445676baea2090117f25fe94bc08263f38f15
ElJorgE23 <s21120186@alumnos.itsur.edu.mx>
2026-03-31 11:33:45 -0600

    Ya se resolvio el detalle para que corra el programa sin detalles

--- MonitoreoDePlagas-main - Copy/build.gradle.kts
+++ MonitoreoDePlagas-main - Copy/build.gradle.kts
@@ -3,26 +3,6 @@
 plugins {
     id("com.android.application") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
+
     id("org.jetbrains.kotlin.kapt") version "1.9.0" apply false
-}
-//A partir de aquí
-buildscript {
-    repositories {
-        // Otras repositorios si es necesario
-        google()
-        mavenCentral()
-        maven(url = "https://maven.pkg.jetbrains.space/public/p/kotlinx-serialization/maven")
-    }
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.2")
-    }
-
-       dependencies {
-
-           // Agrega la dependencia del complemento de Google Services
-           classpath("com.google.gms:google-services") // O la versión más reciente disponible
-       }
-
-
 }
